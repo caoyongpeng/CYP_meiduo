14e2d3bffca5cf834a6d002de78dd23f3025c94c
caoyongpeng <1533392605@qq.com>
2019-11-05 17:47:33 +0800

    日增用户统计

--- meiduo/apps/meiduo_admin/urls.py
+++ meiduo/apps/meiduo_admin/urls.py
@@ -2,9 +2,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from rest_framework_jwt.views import obtain_jwt_token
-
+from .views import statistical
 from . import views
 
 urlpatterns = [
     url(r'^authorizations/$', obtain_jwt_token),
+    url(r'^statistical/total_count/$',statistical.UserTotalCountView.as_view()),
+    url(r'^statistical/day_increment/$',statistical.UserDayCountView.as_view() ),
 ]
---
+++ meiduo/apps/meiduo_admin/views/statistical.py
@@ -0,0 +1,26 @@
+from datetime import date
+
+from rest_framework.views import APIView
+from apps.users.models import User
+from rest_framework.response import Response
+
+
+class UserTotalCountView(APIView):
+    def get(self,request):
+
+        count = User.objects.filter(is_staff=False).count()
+
+        return Response({
+            'count':count
+        })
+
+class UserDayCountView(APIView):
+    def get(self,request):
+
+        now_date = date.today()
+
+        count = User.objects.filter(is_staff=False,date_joined__gte=now_date).count()
+
+        return Response({
+            'count':count
+        })
